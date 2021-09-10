87657c5c05e791571609f8ffa707d134f88d5bfd
giliover <giliover@hotmail.com>
2021-09-10 12:06:49 -0300

    Chore(sort-algorithms): main sort applications

---
+++ sort-algorithms/SORT-ALGORITHMS.c
@@ -0,0 +1,8 @@
+#include "sort-headers/SORT.h"
+
+int main()
+{
+    int integers[length] = {10,  2,  16,  4,  15,  5,  18,  39,  26,  23};
+
+    mergeSort(integers, 0, length - 1); // Pass starting from 0 to Length - 1 of Array
+}
