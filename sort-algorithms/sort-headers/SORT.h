747aaab4ac07690b039c2fd19723d1fc6fce1b0d
giliover <giliover@hotmail.com>
2021-09-11 23:07:08 -0300

    Chore(sort-algorithms): add selectionSort

--- sort-algorithms/sort-headers/SORT.h
+++ sort-algorithms/sort-headers/SORT.h
@@ -10,7 +10,9 @@
 void printArray(int *Integers, int lengthReceveid);
 void countingSort(int *Integers);
 void mergeSort(int *Integers);
+void selectionSort(int *Integers);
 
 #include "CC-PRINT-01.h" /* print int Array */
 #include "CC-COUNT-01.h" /* countingSort algorithm */
 #include "CC-MERGE-01.h" /* mergeSort algorithm */
+#include "CC-SELECTION-01.h" /* selectionSort algorithm */
