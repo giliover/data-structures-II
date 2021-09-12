1b1c205534a90c92ea4df97f2c68996dc670f79f
giliover <giliover@hotmail.com>
2021-09-12 19:08:50 -0300

    Refactor(global-headers): add global headers

---
+++ sort-algorithms/global-headers/GLOBAL.h
@@ -0,0 +1,12 @@
+/*	ADT Prototype Declarations */
+void initArrayWithZero(int *Array, int lengthArray);
+void initArrayWithYourIndex(int *Array, int lengthArray);
+int maxArrayValueIndex(int *Array);
+void printArray(int *Array, int lengthArray);
+void swap(int* firtelement, int* endelement);
+
+#include "CC-SWAP-01.h"  /* Swap between 2 elements */
+#include "CC-INIT-01.h"  /* initialize Array with zero */
+#include "CC-INIT-02.h"  /* init Array with your index */
+#include "CC-MAX-01.h"   /* maximium Array value index */
+#include "CC-PRINT-01.h" /* print Array */
