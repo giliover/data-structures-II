a7527845d88df762a58c4c48daa473fd22f5bd9b
giliover <giliover@hotmail.com>
2021-09-12 20:33:58 -0300

    Chore(sort-algorithms): add algorithms analysis - C file

---
+++ sort-algorithms/init/ALGORITHMS-ANALYSIS.c
@@ -0,0 +1,22 @@
+#include "../search-headers/SEARCH.h"
+#define ISTRING "1 - to binary search algorithms\n\n "
+
+int main()
+{
+    int chosen;
+
+    printf(ISTRING);
+    scanf("%d", &chosen);
+
+    system("clear"); // case your Windows system, set to "cls"
+
+    switch (chosen)
+    {
+    case 1:
+        binarySearch(chosen);
+        break;
+    default:
+        printf("Not found algorithm");
+        break;
+    }
+}
