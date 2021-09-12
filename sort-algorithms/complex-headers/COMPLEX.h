7264bda1135d189b94c0ca082b0965164b5e5cfd
giliover <giliover@hotmail.com>
2021-09-12 20:37:09 -0300

    Fix(complex-headers): change header inclusion priority

--- sort-algorithms/complex-headers/COMPLEX.h
+++ sort-algorithms/complex-headers/COMPLEX.h
@@ -3,7 +3,7 @@ int returnTimeOfBinarySearch(int *Array, int query, int *position);
 void calcComplexByTime(char *FILENAME, int query, int *Array, int *position, int chosen);
 void calcComplexByNumberOfOperations(char *FILENAME, int chosen);
 
-#include "CC-COMPLEX-01.h" /* return time of binary seach */
-#include "CC-COMPLEX-02.h" /* calculates complexity by time */
-#include "CC-COMPLEX-03.h" /* calculates complexity by number of operations */
 #include "CC-COMPLEX-04.h" /* reset file */
+#include "CC-COMPLEX-01.h" /* return time of binary seach */
+#include "CC-COMPLEX-03.h" /* calculates complexity by number of operations */
+#include "CC-COMPLEX-02.h" /* calculates complexity by time */
